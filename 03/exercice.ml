aa4c58f48ff9bdf1182972b60d57c34fc8aa9c30
S0orax <ttreplay@gmail.com>
2014-10-04 15:32:05 +0200

    Réalisation de la fonction qui permet de convertir un fichier encodé en UTF8 vers ISO-8859-1

--- 03/exercice.ml
+++ 03/exercice.ml
@@ -8,22 +8,43 @@
 *)
 
 let isoToUtf e =
-	if e < 160 then 
-		(0, e)
-	else 
-		((e lsr 6) lor 0b11000000, (e lxor 0b11000000) lor 0b10000000)
+	((e lsr 6) lor 0b11000000, (e lxor 0b11000000) lor 0b10000000)
 		
-let isolation_en_utf8 source cible =
+let isolatin_en_utf8 source cible =
 	let entree = open_in source
 	and sortie = open_out cible in
 	try
 		while true do
 			let octet = input_byte entree in
 			if octet < 160 then
-				output_byte sortie (snd (isoToUtf octet))
+				output_byte sortie  octet
 			else begin
 				output_byte sortie (fst (isoToUtf octet));
 				output_byte sortie (snd (isoToUtf octet))
+			end;
+		done
+	with
+		End_of_file ->
+			close_in entree;
+			close_out sortie
+
+let utfToIso o =
+	let fort = (fst o) land 0b00000011 lsl 6
+	and faible = (snd o) land 0b00111111 in
+	faible lor fort
+
+let utf8_en_isolatin source cible =
+	let entree = open_in source
+	and sortie = open_out cible in
+	try
+		while true do
+			let octet = input_byte entree in
+			if octet < 0b11000000 then
+				output_byte sortie octet
+			else
+			begin
+				let octets = (octet, input_byte entree) in
+				output_byte sortie (utfToIso octets)
 			end;
 		done
 	with
