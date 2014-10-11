7de4d60b2b61aedf81d74ce5f5cbea53c9127e1b
S0orax <ttreplay@gmail.com>
2014-10-11 16:01:40 +0200

    Ajout de la documentation

--- 04/codage64.ml
+++ 04/codage64.ml
@@ -20,11 +20,22 @@ table.(42);;
 table.(36);;
 *)
 
+(*
+	reinit_tab -> int array -> unit
+	Reinitialise le tableau a -1
+	parametres :
+		t -> tableau dont on veut reinitialiser les valeurs
+*)
 let reinit_tab t =
 	t.(0) <- -1;
 	t.(1) <- -1;
 	t.(2) <- -1
-
+(*
+	octet_en_sextet -> int array -> char array
+	Renoie en fonction d'un tableau d'octet, un tableau de caractere en focntion du tableau "table"
+	parametres :
+		t -> tableau d'octet, on veut renoyer les caractere associe au codage base64 de ce tableau
+*)
 let octet_en_sextet t = 
 	let tab = [|'='; '='; '='; '=';|] in
 	match t with
@@ -41,21 +52,28 @@ let octet_en_sextet t =
 				tab.(3) <- table.(p land 0b111111);
 				tab
 	|_ -> tab
-
+	
+(*
+	variable de compteur
+*)
 let n = ref 0
 
+(*
+	traiter_octets -> int array -> unit
+	Imprime sur la sortie standard, les caractere en base64 des octets entree en parametres
+	parametres :
+		t -> tableau d'octets
+*)
 let traiter_octets t =
 	match t with
 	| [|-1; -1; -1|] -> Printf.printf "%s" ""
 	| [|_; _; _|] -> 
 		let tab = octet_en_sextet t in
 		Printf.printf "%c%c%c%c" tab.(0) tab.(1) tab.(2) tab.(3);
-		flush stdout;
 		n := !n + 4;
 		if !n mod 76 = 0 then
 		begin
 			Printf.printf "%s" "\n";
-			flush stdout
 		end;
 		reinit_tab t
 	| _ -> Printf.printf "%s" "Mauvais argument"
@@ -77,7 +95,12 @@ let encoder_base64 source =
 		| End_of_file -> 
 			traiter_octets triplet_octets ; 
 			close_in entree
-			
+(*
+	decode_char -> char -> unit
+	renoie l'indice de la table au ce trouve le caractere c
+	parametres :
+		c -> caractere que l'on cherche dans la table
+*)
 let decode_char c =
 	let i = ref 0 in
 	while table.(!i) <> c do
@@ -85,6 +108,12 @@ let decode_char c =
 	done;
 	!i
 
+(*
+	string_en_sextet -> String -> int array
+	renvoie un tableau de longueur 4 d'entier < 64 (sextet)
+	parametres : 
+		s -> Chaine de caractere en base64, chaque caractere a une place dans le tableau que l'on renvoie
+*)
 let string_en_sextet s =
 	let tab_sextet = Array.make 4 0 in
 	for i = 0 to 1 do
@@ -103,7 +132,13 @@ let string_en_sextet s =
 			tab_sextet.(2) <- decode_char c0;
 			tab_sextet.(3) <- decode_char c1;
 			tab_sextet
-		
+
+(*
+	decode_bloc -> String -> int array
+	renvoie un tableau d'entier < 256 (octets) à partir d'une chaine de caracteres en base64
+	parametres :
+		s -> chaine de caracteres en base64 que l'on veut retransformer en tableau d'octets
+*)
 let decode_bloc s =
 	let tab_sextet = string_en_sextet s
 	and tab_octet = Array.make 3 (-1) in
@@ -121,7 +156,13 @@ let decode_bloc s =
 		tab_octet.(2) <- ((p land 0b11) lsl 6) lor q;
 		tab_octet
 	| _ -> tab_octet
-	
+
+(*
+	traiter_bloc -> String -> unit
+	Imprime la chaine en base64 vers l'encodage ISO-8859-1
+	parametres :
+		s -> Chaine de caractere de longueur 4 en base64
+*)
 let traiter_bloc s =
 	let tab_octet = decode_bloc s in
 	match tab_octet with
@@ -130,14 +171,26 @@ let traiter_bloc s =
 	| [| n; m; -1 |] -> Printf.printf "%c%c" (char_of_int n) (char_of_int m)
 	| [| n; m; p |] ->  Printf.printf "%c%c%c" (char_of_int n) (char_of_int m) (char_of_int p)
 	| _ -> Printf.printf "%s" ""
-	
+
+(*
+	traiter_ligne -> String -> unit
+	Imprime le decodage de la ligne en base64 vers ISO-8859-1
+	parametres ; 
+		line -> Chaine de caractere correspondant a une ligne de longueur multiple de 4
+*)
 let traiter_ligne line =
 	let length_line = String.length line in
 	for i = 0 to (length_line - 1) / 4 do
 		let s = String.sub line (i * 4) 4 in
 		traiter_bloc s
 	done
-	
+
+(*
+	decoder_base64 -> String -> unit
+	Imprime le decodage de base64 vers ISO-8859-1 d'un fichier
+	parametres :
+		source : le fichier que l'on veut decoder
+*)
 let decoder_base64 source =
 	let entree = open_in source in
 	try
