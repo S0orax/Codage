e306f9f70b53d1cd907f6f0f48df19c2ffe0e634
S0orax <ttreplay@gmail.com>
2014-10-11 15:38:35 +0200

    TP4 terminé

--- 04/codage64.ml
+++ 04/codage64.ml
@@ -28,26 +28,35 @@ let reinit_tab t =
 let octet_en_sextet t = 
 	let tab = [|'='; '='; '='; '=';|] in
 	match t with
-	|[|_; -1; -1|] -> tab.(0) <- table.(t.(0) lsr 2); 
-				tab.(1) <- table.((t.(0) land 0b11) lsl 4);
+	|[|n; -1; -1|] -> tab.(0) <- table.(n lsr 2); 
+				tab.(1) <- table.((n land 0b11) lsl 4);
 				tab
-	|[|_; _; -1|] -> tab.(0) <- table.(t.(0) lsr 2); 
-				tab.(1) <- table.(((t.(0) land 0b11) lsl 4) lor (t.(1) lsr 4)); 
-				tab.(2) <- table.((t.(1) land 0b1111) lsl 2);
+	|[|n; m; -1|] -> tab.(0) <- table.(n lsr 2); 
+				tab.(1) <- table.(((n land 0b11) lsl 4) lor (m lsr 4)); 
+				tab.(2) <- table.((m land 0b1111) lsl 2);
 				tab
-	|[|_; _; _|] -> tab.(0) <- table.(t.(0) lsr 2); 
-				tab.(1) <- table.(((t.(0) land 0b11) lsl 4) lor (t.(1) lsr 4)); 
-				tab.(2) <- table.(((t.(1) land 0b1111) lsl 2) lor (t.(2) lsr 6));
-				tab.(3) <- table.(t.(2) land 0b111111);
+	|[|n; m; p|] -> tab.(0) <- table.(n lsr 2); 
+				tab.(1) <- table.(((n land 0b11) lsl 4) lor (m lsr 4)); 
+				tab.(2) <- table.(((m land 0b1111) lsl 2) lor (p lsr 6));
+				tab.(3) <- table.(p land 0b111111);
 				tab
 	|_ -> tab
 
+let n = ref 0
+
 let traiter_octets t =
 	match t with
-	| [|-1; -1; -1|] -> Printf.printf ""
+	| [|-1; -1; -1|] -> Printf.printf "%s" ""
 	| [|_; _; _|] -> 
 		let tab = octet_en_sextet t in
-		Printf.printf "%c%c%c%c" tab.(0) tab.(1) tab.(2) tab.(3); 
+		Printf.printf "%c%c%c%c" tab.(0) tab.(1) tab.(2) tab.(3);
+		flush stdout;
+		n := !n + 4;
+		if !n mod 76 = 0 then
+		begin
+			Printf.printf "%s" "\n";
+			flush stdout
+		end;
 		reinit_tab t
 	| _ -> Printf.printf "%s" "Mauvais argument"
 
@@ -68,3 +77,74 @@ let encoder_base64 source =
 		| End_of_file -> 
 			traiter_octets triplet_octets ; 
 			close_in entree
+			
+let decode_char c =
+	let i = ref 0 in
+	while table.(!i) <> c do
+		incr i
+	done;
+	!i
+
+let string_en_sextet s =
+	let tab_sextet = Array.make 4 0 in
+	for i = 0 to 1 do
+		tab_sextet.(i) <- decode_char s.[i]
+	done;
+	match (s.[2], s.[3]) with
+		| ('=', '=') -> 
+			tab_sextet.(2) <- (-1);
+			tab_sextet.(3) <- (-1);
+			tab_sextet
+		| (c, '=') ->
+			tab_sextet.(2) <- decode_char c;
+			tab_sextet.(3) <- (-1);
+			tab_sextet
+		| (c0, c1) -> 
+			tab_sextet.(2) <- decode_char c0;
+			tab_sextet.(3) <- decode_char c1;
+			tab_sextet
+		
+let decode_bloc s =
+	let tab_sextet = string_en_sextet s
+	and tab_octet = Array.make 3 (-1) in
+	match tab_sextet with
+	| [| n; m; (-1); (-1) |] ->
+		tab_octet.(0) <- (n lsl 2) lor (m lsr 4);
+		tab_octet
+	| [| n; m; p; (-1) |] ->
+		tab_octet.(0) <- (n lsl 2) lor (m lsr 4);
+		tab_octet.(1) <- ((m land 0b1111) lsl 4) lor (p lsr 4);
+		tab_octet
+	| [| n; m; p; q |] ->
+		tab_octet.(0) <- (n lsl 2) lor (m lsr 4);
+		tab_octet.(1) <- ((m land 0b1111) lsl 4) lor (p lsr 2);
+		tab_octet.(2) <- ((p land 0b11) lsl 6) lor q;
+		tab_octet
+	| _ -> tab_octet
+	
+let traiter_bloc s =
+	let tab_octet = decode_bloc s in
+	match tab_octet with
+	| [| -1; -1; -1 |] -> Printf.printf "%s" ""
+	| [| n; -1; -1 |] -> Printf.printf "%c" (char_of_int n)
+	| [| n; m; -1 |] -> Printf.printf "%c%c" (char_of_int n) (char_of_int m)
+	| [| n; m; p |] ->  Printf.printf "%c%c%c" (char_of_int n) (char_of_int m) (char_of_int p)
+	| _ -> Printf.printf "%s" ""
+	
+let traiter_ligne line =
+	let length_line = String.length line in
+	for i = 0 to (length_line - 1) / 4 do
+		let s = String.sub line (i * 4) 4 in
+		traiter_bloc s
+	done
+	
+let decoder_base64 source =
+	let entree = open_in source in
+	try
+		while true do
+			let s = input_line entree in
+			traiter_ligne s
+		done
+	with
+		End_of_file ->
+			close_in entree;
